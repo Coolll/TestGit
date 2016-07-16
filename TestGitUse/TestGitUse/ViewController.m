ba297527b5fe7950b85bd0b88e94eafc52c76205
WQL WQL <2502127576@qq.com>
2016-07-16 14:47:57 +0800

    add testMethod

--- TestGitUse/TestGitUse/ViewController.m
+++ TestGitUse/TestGitUse/ViewController.m
@@ -8,6 +8,11 @@
 
 #import "ViewController.h"
 
+#define PhoneScreen_HEIGHT [UIScreen mainScreen].bounds.size.height
+#define PhoneScreen_WIDTH [UIScreen mainScreen].bounds.size.width
+#define RGBCOLOR(r,g,b,_alpha) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:_alpha]
+
+
 @interface ViewController ()
 
 @property (nonatomic,strong) NSArray *dataArray;
@@ -21,8 +26,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.view.backgroundColor = [UIColor lightGrayColor];
+    self.view.backgroundColor = [UIColor whiteColor];
     
+    [self testTwo];
+    
+}
+
+- (void)testOne
+{
     NSString *testString = @"-1";
     
     NSInteger interValue = [testString integerValue];
@@ -30,6 +41,32 @@
     if (interValue) {
         NSLog(@"进入判断");
     }
+}
+
+
+- (void)testTwo
+{
+    UILabel *oldPriceLabel = [[UILabel alloc]initWithFrame:CGRectMake((PhoneScreen_WIDTH-200)/2, 100, 200, 50)];
+    oldPriceLabel.backgroundColor = [UIColor lightGrayColor];
+    oldPriceLabel.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:oldPriceLabel];
+    
+    
+    NSString *oldPriceString = @"¥ 2016.0716";
+    
+    NSUInteger length = [oldPriceString length];
+    
+    //构建attributeString
+    NSMutableAttributedString *attri = [[NSMutableAttributedString alloc] initWithString:oldPriceString];
+    
+    //删除线的range
+    [attri addAttribute:NSStrikethroughStyleAttributeName value:@(NSUnderlinePatternSolid | NSUnderlineStyleSingle) range:NSMakeRange(2, length-2)];
+    
+    //删除线的颜色
+    [attri addAttribute:NSStrikethroughColorAttributeName value:RGBCOLOR(243, 16, 36, 1.0) range:NSMakeRange(2, length-2)];
+    
+    //设置富文本
+    [oldPriceLabel setAttributedText:attri];
     
 }
 
