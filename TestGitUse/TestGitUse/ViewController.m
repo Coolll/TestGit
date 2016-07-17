211649859296d8820e5fe9180d8abfe26d4aa3f4
WQL WQL <2502127576@qq.com>
2016-07-17 17:24:06 +0800

    add test alertView

--- TestGitUse/TestGitUse/ViewController.m
+++ TestGitUse/TestGitUse/ViewController.m
@@ -13,11 +13,14 @@
 #define RGBCOLOR(r,g,b,_alpha) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:_alpha]
 
 
-@interface ViewController ()
+@interface ViewController ()<UITextFieldDelegate>
 
 @property (nonatomic,strong) NSArray *dataArray;
 
 @property (nonatomic,assign) NSInteger age;
+
+@property (nonatomic,strong) UITextField *testTextField;
+
 
 @end
 
@@ -28,7 +31,8 @@
     
     self.view.backgroundColor = [UIColor whiteColor];
     
-    [self testTwo];
+
+    [self testThree];
     
 }
 
@@ -69,6 +73,57 @@
     [oldPriceLabel setAttributedText:attri];
     
 }
+
+- (void)testThree
+{
+    self.testTextField = [[UITextField alloc]initWithFrame:CGRectMake((PhoneScreen_WIDTH-200)/2, 150, 200, 40)];
+    self.testTextField.backgroundColor = RGBCOLOR(240, 240, 240, 1.0);
+    self.testTextField.text = @"This is just a testString~";
+    self.testTextField.delegate = self;
+    self.testTextField.returnKeyType = UIReturnKeyDone;
+    [self.view addSubview:self.testTextField];
+}
+
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+    if ([string isEqualToString:@"\n"]) {
+        [textField resignFirstResponder];
+
+        return NO;
+    }else{
+        return YES;
+    }
+}
+
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+//    [self showAbandonAlert];
+    [self showAlertController];
+    
+}
+
+//废弃的alertView
+- (void)showAbandonAlert
+{
+    UIAlertView *alertView = [[UIAlertView alloc]initWithTitle:@"提示" message:@"结束编辑啦" delegate:self cancelButtonTitle:@"确定" otherButtonTitles:nil, nil];
+    [alertView show];
+}
+
+
+//alertController
+- (void)showAlertController
+{
+    UIAlertController *controller = [UIAlertController alertControllerWithTitle:@"提示" message:@"结束编辑啦" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *action = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        NSLog(@"确定Action");
+    }];
+    [controller addAction:action];
+    
+    [self presentViewController:controller animated:YES completion:nil];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
